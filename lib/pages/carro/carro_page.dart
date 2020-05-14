12d1d79926201fecb9ba1cd3937720bb3fc714da
Bruno Glauco Eles <bgeles@gmail.com>
2020-05-14 14:29:14 -0300

    adicionando popmenu no carro detalhes

--- lib/pages/carro/carro_page.dart
+++ lib/pages/carro/carro_page.dart
@@ -12,9 +12,58 @@ class CarroPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(carro.nome),
+        actions: <Widget>[
+          IconButton(
+            onPressed: _onClickMapa,
+            icon: Icon(
+              Icons.place,
+            ),
+          ),
+          IconButton(
+            onPressed: _onClickVideo,
+            icon: Icon(
+              Icons.video_call,
+            ),
+          ),
+          PopupMenuButton<String>(
+            onSelected: _onClickPopupMenu,
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(
+                  value: "Editar",
+                  child: Text("Editar"),
+                ),
+                PopupMenuItem(
+                  value: "Deletar",
+                  child: Text("Deletar"),
+                ),
+                PopupMenuItem(
+                  value: "Share",
+                  child: Text("Share"),
+                ),
+              ];
+            },
+          )
+        ],
       ),
       body: _body(),
     );
+  }
+
+  void _onClickPopupMenu(String value) {
+
+    switch(value){
+      case "Editar":
+      print("Editar !!!");
+      break;
+      case "Deletar":
+      print("Deletar !!!");
+      break;
+      case "Share":
+      print("Share !!!");
+      break;
+    }
+
   }
 
   _body() {
@@ -23,4 +72,8 @@ class CarroPage extends StatelessWidget {
       child: Image.network(carro.urlFoto),
     );
   }
+
+  void _onClickMapa() {}
+
+  void _onClickVideo() {}
 }
