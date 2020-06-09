01b216a15d16edbfaf9f606b4abdd4a1ec720553
Bruno Glauco Eles <bgeles@gmail.com>
2020-06-08 22:33:34 -0300

    atualização

--- lib/pages/carros/carros_api.dart
+++ lib/pages/carros/carros_api.dart
@@ -42,8 +42,6 @@ class CarrosApi {
 
     final carros = list.map<Carro>((map) => Carro.fromMap(map)).toList();
 
-    
-
     return carros;
 
     //} catch (error) {
@@ -52,37 +50,49 @@ class CarrosApi {
   }
 
   static Future<ApiResponse<bool>> save(Carro c) async {
-    Usuario user = await Usuario.get();
+    try {
+      Usuario user = await Usuario.get();
 
-    Map<String,String> headers = {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer ${user.token}"
-    };
+      Map<String, String> headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer ${user.token}"
+      };
 
-    var url = 'https://carros-springboot.herokuapp.com/api/v2/carros';
+      var url = 'https://carros-springboot.herokuapp.com/api/v2/carros';
+      if (c.id != null) {
+        url += "/${c.id}";
+      }
+      print("POST > $url");
 
-    print("POST > $url");
+      String json = c.toJson();
 
-    String json = c.toJson();
+      print("   JSON > $json");
 
-    print("   JSON > $json");
+      var response = await (c.id == null
+          ? http.post(url, body: json, headers: headers)
+          : http.put(url, body: json, headers: headers));
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
 
-    var response = await http.post(url, body: json, headers: headers);
+      if (response.statusCode == 201 || response.statusCode == 200) {
+        Map mapResponse = convert.json.decode(response.body);
 
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
+        Carro carro = Carro.fromMap(mapResponse);
 
-    if(response.statusCode == 201) {
+        print("Novo carro: ${carro.id}");
+
+        return ApiResponse.ok(true);
+      }
+
+      if (response.body == null || response.body.isEmpty) {
+        return ApiResponse.error("Não foi possível salvar o carro");
+      }
+
       Map mapResponse = convert.json.decode(response.body);
-
-      Carro carro = Carro.fromMap(mapResponse);
-
-      print("Novo carro: ${carro.id}");
-
-      return ApiResponse.ok(true);
+      return ApiResponse.error(mapResponse["error"]);
+    } catch (e) {
+      print(e);
+      return ApiResponse.error("Não foi possível salvar o carro");
     }
-
-    Map mapResponse = convert.json.decode(response.body);
-    return ApiResponse.error(mapResponse["error"]);
   }
 }
